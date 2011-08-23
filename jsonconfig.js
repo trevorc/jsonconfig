e29f40414c6ef20847ba113b67b999eeefd8f573
Trevor Caira <trevor@caira.com>
2011-08-23 19:02:14 -0400

    Merge objects at the same key path.

--- jsonconfig.js
+++ jsonconfig.js
@@ -1,9 +1,20 @@
 var fs = require('fs');
 var config = this;
 
+function isObject(o) {
+  return o && o.constructor === Object;
+}
+
+function extend(dst, src, when) {
+  for (k in src) if (src.hasOwnProperty(k)) {
+    if (isObject(dst[k]) && isObject(dst[k])) {
+      extend(dst[k], src[k]);
+    } else dst[k] = src[k];
+  }
+}
+
 exports.load = function(paths) {
   (paths || []).forEach(function(x) {
-    var obj = JSON.parse(fs.readFileSync(x, 'UTF-8'));
-    for (k in obj) if (obj.hasOwnProperty(k)) config[k] = obj[k];
+    extend(config, JSON.parse(fs.readFileSync(x, 'UTF-8')));
   });
 };
